79bed293ad647bcb2e47e6680147652d87d9ab33
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:51:01 +0300

    pom update

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -5,7 +5,6 @@ import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
